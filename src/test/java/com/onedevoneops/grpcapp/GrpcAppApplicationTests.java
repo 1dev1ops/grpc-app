36b2882b7171347b0ec49318e56ff818f652cddd
Fehim Erdogan <fehim.erdogan@gmail.com>
2020-07-04 10:28:52 +0300

    Skeleton of the project

---
+++ src/test/java/com/onedevoneops/grpcapp/GrpcAppApplicationTests.java
@@ -0,0 +1,12 @@
+package com.onedevoneops.grpcapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcAppApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
