e5df662ffbd7d78151c8e97898d97e64b28ba02c
Mert Acikportali <mert.acikportali@trivago.com>
2020-07-06 21:48:04 +0200

    Remove grpc-server and add blackhole

--- client/src/main/java/com/onedevoneops/client/benchmark/ExecutionPlan.java
+++ client/src/main/java/com/onedevoneops/client/benchmark/ExecutionPlan.java
@@ -4,7 +4,6 @@ import com.onedevoneops.grpcserver.service.PayloadTestServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import org.openjdk.jmh.annotations.Level;
-import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
@@ -13,20 +12,17 @@ import org.springframework.web.client.RestTemplate;
 /**
  * @author erdoganf
  */
-@State(Scope.Benchmark)
+@State(Scope.Thread)
 public class ExecutionPlan {
 
   private static final ManagedChannel CHANNEL =
       ManagedChannelBuilder.forAddress("localhost", 8080).usePlaintext().build();
 
-  @Param({"1"})
-  public int iterations;
-
   public RestTemplate restTemplate;
 
   public PayloadTestServiceGrpc.PayloadTestServiceBlockingStub stub;
 
-  @Setup(Level.Iteration)
+  @Setup(Level.Trial)
   public void setUp() {
     stub = PayloadTestServiceGrpc.newBlockingStub(CHANNEL);
 
--- client/src/main/java/com/onedevoneops/client/benchmark/ServerBenchmark.java
+++ client/src/main/java/com/onedevoneops/client/benchmark/ServerBenchmark.java
@@ -9,6 +9,7 @@ import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.infra.Blackhole;
 import org.springframework.http.ResponseEntity;
 
 /**
@@ -19,15 +20,16 @@ public class ServerBenchmark {
   @Benchmark
   @BenchmarkMode(Mode.AverageTime)
   @OutputTimeUnit(TimeUnit.MILLISECONDS)
-  public void benchmarkGrpc(ExecutionPlan plan) {
+  public void benchmarkGrpc(ExecutionPlan plan, Blackhole blackhole) {
     HeroNameResponse smallPayload = plan.stub.getSmallPayload(Empty.newBuilder().build());
+    blackhole.consume(smallPayload);
   }
 
   @Benchmark
   @BenchmarkMode(Mode.AverageTime)
   @OutputTimeUnit(TimeUnit.MILLISECONDS)
-  public void benchmarkRest(ExecutionPlan plan) {
-    String url = "http://localhost:8081/api/payloads/heroes";
-    ResponseEntity<GetHeroNames> smallPayloadRest = plan.restTemplate.getForEntity(url, GetHeroNames.class);
+  public void benchmarkRest(ExecutionPlan plan, Blackhole blackhole) {
+    ResponseEntity<GetHeroNames> smallPayloadRest = plan.restTemplate.getForEntity("http://localhost:8081/api/payloads/heroes", GetHeroNames.class);
+    blackhole.consume(smallPayloadRest);
   }
 }
