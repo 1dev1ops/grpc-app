e42c6aec24d69581ffc28e7e67fdfb90c9948ccf
Hmerac <mertacikportali@gmail.com>
2020-07-06 22:05:30 +0200

    Add large payload benchmarks

--- client/src/main/java/com/onedevoneops/client/benchmark/ServerBenchmark.java
+++ client/src/main/java/com/onedevoneops/client/benchmark/ServerBenchmark.java
@@ -2,11 +2,14 @@ package com.onedevoneops.client.benchmark;
 
 import com.google.protobuf.Empty;
 import com.onedevoneops.grpcserver.service.HeroNameResponse;
+import com.onedevoneops.grpcserver.service.TeamFight;
+import com.onedevoneops.grpcserver.service.TeamFightResponse;
 import com.onedevoneops.springrestapp.bean.request.GetHeroNames;
+import com.onedevoneops.springrestapp.bean.request.GetTeamFights;
+
 import java.util.concurrent.TimeUnit;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.infra.Blackhole;
@@ -18,18 +21,34 @@ import org.springframework.http.ResponseEntity;
 public class ServerBenchmark {
 
   @Benchmark
-  @BenchmarkMode(Mode.AverageTime)
+  @BenchmarkMode(Mode.All)
   @OutputTimeUnit(TimeUnit.MILLISECONDS)
-  public void benchmarkGrpc(ExecutionPlan plan, Blackhole blackhole) {
+  public void benchmarkSmallPayloadGrpc(ExecutionPlan plan, Blackhole blackhole) {
     HeroNameResponse smallPayload = plan.stub.getSmallPayload(Empty.newBuilder().build());
     blackhole.consume(smallPayload);
   }
 
   @Benchmark
-  @BenchmarkMode(Mode.AverageTime)
+  @BenchmarkMode(Mode.All)
   @OutputTimeUnit(TimeUnit.MILLISECONDS)
-  public void benchmarkRest(ExecutionPlan plan, Blackhole blackhole) {
+  public void benchmarkSmallPayloadRest(ExecutionPlan plan, Blackhole blackhole) {
     ResponseEntity<GetHeroNames> smallPayloadRest = plan.restTemplate.getForEntity("http://localhost:8081/api/payloads/heroes", GetHeroNames.class);
     blackhole.consume(smallPayloadRest);
+  }
+
+  @Benchmark
+  @BenchmarkMode(Mode.All)
+  @OutputTimeUnit(TimeUnit.MILLISECONDS)
+  public void benchmarkLargePayloadGrpc(ExecutionPlan plan, Blackhole blackhole) {
+    TeamFightResponse largePayload = plan.stub.getLargePayload(Empty.newBuilder().build());
+    blackhole.consume(largePayload);
+  }
+
+  @Benchmark
+  @BenchmarkMode(Mode.All)
+  @OutputTimeUnit(TimeUnit.MILLISECONDS)
+  public void benchmarkLargePayloadRest(ExecutionPlan plan, Blackhole blackhole) {
+    ResponseEntity<GetTeamFights> largePayloadRest = plan.restTemplate.getForEntity("http://localhost:8081/api/payloads/teamfights", GetTeamFights.class);
+    blackhole.consume(largePayloadRest);
   }
 }
